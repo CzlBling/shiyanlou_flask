a6908d09e0f8f9d791a966bf0c512dcf9cef57f2
CzlBling <474916985@qq.com>
2017-12-27 10:26:57 +0800

    new test

--- news/app.py
+++ news/app.py
@@ -1,28 +1,54 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
-import os
-import json
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql"//root@localhost/shiyanlou'
+db = SQLAlchemy（app）
+
+class File(db.Model):
+	__tablename__ = 'files'
+	id = db.Column(db.Integer, primary_key = True)
+	title = db.Column(db.String(80))
+	created_time = db.Column(db.DateTime)
+	category_id = db.Column(db.Integer, db.ForeignKey(categorys.id))
+	category = db.relationship('Category', uselist = False)
+	content = db.Column(db.Text)
+
+	def __init__(self, title, created_time, category, content):
+		self.title = title
+		self.created_time = created_time
+		self.category = category
+		self.content = content
+
+class Category(db.Model):
+	__tablename__ = 'categorys'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(80))
+	files = db.relationship('File')
+
+	def __init__(self, name):
+		self.name = name
+
+def write_datas():
+	java = Category('Java')
+	python = Category('Python')
+	file1 = File('hello Java', datetime.utcnow(), java, 'File Content - Java is cool!')
+	file2 = File('hello Python', datetime.utcnow(), java, 'File Content - Python is cool!')
+	db.session.add(java)
+	db.session.add(python)
+	db.session.add(file1)
+	db.session.add(file2)
+	db.session.commit()
 
 @app.route('/')
 def index():
-	listdir = os.listdir('/home/shiyanlou/files/')
-	index_dir = []
-	for i in listdir:
-		with open('/home/shiyanlou/files/' + i, 'r') as json_what:
-			b = json.loads(json_what.read())
-		index_dir.append(b['title'])
-	return render_template('index.html', index_dir=index_dir)
 
-@app.route('/files/<name>')
-def files(name):
-	if os.path.isfile('/home/shiyanlou/files/' + name + '.json'):
-		with open('/home/shiyanlou/files/' + name + '.json', 'r') as json_what:
-			a = json.loads(json_what.read())
-		return render_template('base.html', a=a)
-	else:	
-		return render_template('404.html'), 404
+
+@app.route('/files/<file_id>')
+def files(file_id):
+
 
 @app.errorhandler(404)
 def not_found(error):
